9606e8751fa01fb8f5b5cafe9beffcc9b65be195
Cindy Cicalese <ccicalese@wikimedia.org>
2020-05-31 19:45:48 +0000

    Allow phan to run locally as well as in Wikimedia CI

--- .phan/config.php
+++ .phan/config.php
@@ -1,2 +1,31 @@
 <?php
-return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$IP = getenv( 'MW_INSTALL_PATH' ) !== false
+	? str_replace( '\\', '/', getenv( 'MW_INSTALL_PATH' ) )
+	: __DIR__ . '/../../..';
+
+$scss_dir = $IP . "/vendor/mediawiki/scss";
+
+if ( !file_exists( $scss_dir ) ) {
+	$scss_dir = __DIR__ . '/../vendor/mediawiki/scss';
+}
+
+if ( file_exists( $scss_dir ) ) {
+
+	$cfg['directory_list'] = array_merge(
+		$cfg['directory_list'],
+		[
+			$scss_dir,
+		]
+	);
+
+	$cfg['exclude_analysis_directory_list'] = array_merge(
+		$cfg['exclude_analysis_directory_list'],
+		[
+			$scss_dir,
+		]
+	);
+}
+
+return $cfg;
